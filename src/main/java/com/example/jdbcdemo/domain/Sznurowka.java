094168ce7ee7503d8ebf9d5f7b87518964645df3
student <student@student-vm.(none)>
2012-04-27 11:32:32 +0200

    1st

---
+++ src/main/java/com/example/jdbcdemo/domain/Sznurowka.java
@@ -0,0 +1,55 @@
+package com.example.jdbcdemo.domain;
+
+public class Sznurowka {
+	
+	private Long id;
+	
+	private String producent;
+	
+	private int dlugosc;
+	
+	private String kolor;
+	
+	private int grubosc;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getProducent() {
+		return producent;
+	}
+
+	public void setProducent(String producent) {
+		this.producent = producent;
+	}
+
+	public int getDlugosc() {
+		return dlugosc;
+	}
+
+	public void setDlugosc(int dlugosc) {
+		this.dlugosc = dlugosc;
+	}
+
+	public String getKolor() {
+		return kolor;
+	}
+
+	public void setKolor(String kolor) {
+		this.kolor = kolor;
+	}
+
+	public int getGrubosc() {
+		return grubosc;
+	}
+
+	public void setGrubosc(int grubosc) {
+		this.grubosc = grubosc;
+	}
+	
+}
